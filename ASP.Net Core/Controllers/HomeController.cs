35ed100c187d003d27744036b171d42ec2138c0b
Marah <marah.hazim@gmail.com>
2026-01-26 14:38:31 +0100

    Ajout ASP.NET

---
+++ ASP.Net Core/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using DemoAPIs.ApiHello.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPIs.ApiHello.Controllers
+{
+    [ApiController]
+    [Route("api/v1/accueil")] // monsite.com/api/v1/accueil
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        // monsite.com/api/v1/accueil
+        public string HelloWorld()
+        {
+            return "Hello world";
+        }
+
+        [HttpGet("someone/{fullname}")]
+        // monsite.com/api/v1/accueil/someone/:fullname
+        public string HelloSomeone(string fullname)
+        {
+            return $"Hello {fullname}";
+        }
+
+        [HttpPost("someone")]
+        // monsite.com/api/v1/accueil/someone
+
+        /*
+            {
+                "firstname": "Antoine", 
+                "lastname": "Dieudonne",
+                "team": "Development"
+            }
+         */
+        public string HelloSomeonePost([FromBody] SomeoneRequestRecord payload)
+        {
+            return $"Hello {payload.Firstname} {payload.Lastname}, you're working in team: {payload.Team}";
+        }
+    }
+}
---
+++ ASP.Net Core/DTOs/SomeoneRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace DemoAPIs.ApiHello.DTOs
+{
+    public class SomeoneRequest
+    {
+        private string _firstname;
+
+        public string Firstname
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        // GET / SET fonctionnera mais peut amener à des soucis de modification de termes de recherches
+        public string Lastname { get; private set; }
+
+        // On privilégie donc GET / INIT de sorte à ne pas pouvoir modifier les valeurs de recherches 
+        public string Team { get; init; }
+
+        public SomeoneRequest(string firstname)
+        {
+            _firstname = firstname;
+        }
+    }
+
+    // Normalement, pour faire des DTOs, on privilégie un mécanisme non modifiable, 'immutable'. Pour cela, le plus simple,
+    // depuis le C# 7+, est d'utiliser les records
+    public record SomeoneRequestRecord(string Firstname, string Lastname, string Team);
+}
